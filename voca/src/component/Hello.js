a94d82b0f8ebbba1362f07f92b7a3ff5585dd554
donggeun <dgsim126@gmail.com>
2025-06-27 19:52:30 +0900

    feat: button input

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,12 +1,24 @@
 import World from "./World";
 
 const Hello= function(){
+    function showName(){
+        console.log("Donggeun");
+    }
+    function showAge(age){
+        console.log(age)
+    }
+    function showText(e){
+        console.log(e.target.value)
+    }
+
     return (
         <div>
             <h1>Hello</h1>
-            <World />
-            <World />
-        </div>
+            <button onClick={showName}>Show name</button>
+            <button onClick={() => {showAge(10);}}>Show age</button>
+
+            <input type="text" onChange={showText}/>
+        </div> 
     
     )
 }
