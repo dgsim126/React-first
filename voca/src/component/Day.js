5b42068b852bcd98f45a992f69fcdb511630b869
donggeun <dgsim126@gmail.com>
2025-07-02 14:05:21 +0900

    learn: Custom Hooks

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -2,21 +2,24 @@ import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
 import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Day(){
     const a= useParams();
     const day= a.day;
-    const [words, setWords]= useState([]);
+    // const [words, setWords]= useState([]);
 
-    useEffect(()=>{
-        fetch(`http://localhost:3001/words?day=${day}`)
-        .then(res=>{
-            return res.json()
-        })
-        .then(data=>{
-            setWords(data)
-        })
-    }, [day])
+    // useEffect(()=>{
+    //     fetch(`http://localhost:3001/words?day=${day}`)
+    //     .then(res=>{
+    //         return res.json()
+    //     })
+    //     .then(data=>{
+    //         setWords(data)
+    //     })
+    // }, [day])
+
+    const words= useFetch(`http://localhost:3001/words?day=${day}`)
 
     return ( <>
         <h2>Day {day}</h2>
