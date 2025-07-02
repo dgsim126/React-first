5c505b8d5880e537fa177e3c01ccdc3e77bc9a85
donggeun <dgsim126@gmail.com>
2025-07-02 17:08:55 +0900

    learn: POST, navigate

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Day from './component/Day';
 import DayList from './component/DayList';
+import CreateWord from './component/CreateWord.js'
+import CreateDay from './component/CreateDay.js'
 import EmptyPage from './component/EmptyPage';
 import Header from './component/Header';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path='/create_word' element={<CreateWord />} />
+          <Route path='/create_day' element={<CreateDay />} />
           <Route path="*" element={<EmptyPage />}></Route>
         </Routes>
       </div>
--- voca/src/hooks/useFetch.js
+++ voca/src/hooks/useFetch.js
@@ -16,3 +16,4 @@ export default function useFetch(url){
     
     return data
 }
+
